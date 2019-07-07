70ca585d35157be02753184ec9c50129de94c604
Grigor Temejikyan <volo.grigor.temejikyan@gmail.com>
2019-07-07 12:18:52 +0400

    Added DataBase project.

---
+++ Chat/Chat.Database/dbo/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [UserName] NVARCHAR (50)  NOT NULL,
+    [Password] NVARCHAR (500) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
